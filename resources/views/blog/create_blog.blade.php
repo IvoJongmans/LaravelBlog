7e7ece84056664decd074104d0c18681f6401955
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-03 14:06:59 +0200

    First commit

---
+++ resources/views/blog/create_blog.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('content')
+    <h1>Create a blog entry!</h1>
+
+    <form method="POST" action="/blog">
+        @csrf
+
+        <div>
+            <input type="text" name="blog_title" placeholder="Title">
+        </div>
+        <div>
+            <textarea name="blog_body" placeholder="Body"></textarea>
+        </div>
+        <div>
+            <button type="submit">CREATE!</button>
+        </div>
+    </form>
+
+@endsection
+
---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+
+@section('content')
+
+<h1>Welcome to our blog!</h1>
+
+<a href="blog/create"><button type="button">CREATE ENTRY</button></a>
+
+@foreach($allBlogs as $blog)
+
+<div>
+    <h2>{{$blog->blog_title}}</h2>
+
+    <p>{{$blog->blog_body}}</p>
+
+    <p>{{$blog->created_at}}</p>
+
+</div>
+
+@endforeach
+    
+@endsection
