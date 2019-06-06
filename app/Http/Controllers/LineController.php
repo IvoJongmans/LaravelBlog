92d03d4d232de1a30bd556e974cc4fed2860701d
Ivo Jongmans <ivojongmans@gmail.com>
2019-06-06 16:30:25 +0200

    added exception for linehook to the middleware

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -11,7 +11,64 @@ class LineController extends Controller
 
 
     public function event(Request $request) {
-        
+        $body 	   = file_get_contents('php://input');
+$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+
+	// log body and signature
+	file_put_contents('php://stderr', 'Body: '.$body);
+
+	// is LINE_SIGNATURE exists in request header?
+	if (empty($signature)){
+		return $response->withStatus(400, 'Signature not set');
+	}
+
+	// is this request comes from LINE?
+	if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
+		return $response->withStatus(400, 'Invalid signature');
+	}
+
+	// init bot
+	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+	$data = json_decode($body, true);
+	foreach ($data['events'] as $event)
+	{
+		$userMessage = $event['message']['text'];
+		if(strtolower($userMessage) == 'hallo')
+		{
+			$message = "Hallo Gorilla";
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		
+		}
+
+
+	   if(strtolower($userMessage) == 'afspraak')
+		{
+			$message = "Kun je vanmiddag om 15:00 ?";
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		
+		}
+
+
+     if(strtolower($userMessage) == 'sticker')
+		{
+	
+            $mysticker = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder("11538", "51626501");
+			$result = $bot->replyMessage($event['replyToken'], $mysticker);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		
+		}
+
+
+
+
+
+
+	}
 
 
    
